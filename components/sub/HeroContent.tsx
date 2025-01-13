2c8af2c5d8d1b6bbe9b8aea49967e296282f33a4
batufidan <batuhan.fidan@ogr.iu.edu.tr>
2025-01-14 00:07:21 +0300

    [ADD] some features

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -17,13 +17,13 @@ const HeroContent = () => {
       animate="visible"
       className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
     >
-      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto md:text-start text-center">
+      <div className="h-full w-full flex flex-col gap-5 justify-center  m-auto md:text-start text-center">
         <motion.div
           variants={slideInFromTop}
-          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9] flex justify-center sm:justify-start items-center sm:ml-0 ml-4"
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">
+          <h1 className="Welcome-text text-[13px] text-center sm:text-left">
             Araç & Nesne Takip Sistemi
           </h1>
         </motion.div>
